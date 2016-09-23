ca9ae0fa72cdb7d34e2eb9c60a634453daf2b8b7
Wei Song <wsong83@gmail.com>
2016-09-23 15:35:58 +0100

    finish tag cache base class

--- riscv/tagcachesim.cc
+++ riscv/tagcachesim.cc
@@ -1,10 +1,15 @@
 // See LICENSE for license details.
 
 #include "tagcachesim.h"
+#include <cassert>
+#include <cstring>
+#include <cassert>
 
 // ----------------- tag cache base class ----------------------- //
 
-tag_cache_sim_t::tag_cache_sim_t(size_t sets, size_t ways, size_t linesz, size_t tagsz, const char* name, const sim_t* sim)
+static uint8_t *empty_block = NULL;
+
+tag_cache_sim_t::tag_cache_sim_t(size_t sets, size_t ways, size_t linesz, size_t tagsz, const char* name, sim_t* sim)
   : cache_sim_t(sets, ways, linesz, name), tagsz(tagsz), sim(sim)
 {
   init();
@@ -23,23 +28,129 @@ tag_cache_sim_t::~tag_cache_sim_t() {
 
 void tag_cache_sim_t::init() {
   datas = new uint8_t[sets*ways*linesz]();
+  if(empty_block == NULL) empty_block = new uint8_t[linesz]();
+}
+
+uint64_t* tag_cache_sim_t::check_tag(uint64_t addr, size_t &row) {
+  size_t idx = (addr >> idx_shift) & (sets-1);
+  size_t tag = (addr >> idx_shift) | VALID;
+
+  for (size_t i = 0; i < ways; i++)
+    if (tag == (tags[idx*ways + i] & ~(DIRTY|TAGFLAG))) {
+      row = idx*ways + i;
+      return &tags[idx*ways + i];
+    }
+  return NULL;
+}
+
+uint64_t* tag_cache_sim_t::victimize(uint64_t addr, size_t &row) {
+  size_t idx = (addr >> idx_shift) & (sets-1);
+  size_t way = lfsr.next() % ways;
+  row = idx*ways + way;
+  return &tags[row];
+}
+
+void tag_cache_sim_t::refill(uint64_t addr, size_t row) {
+  memcpy(datas + row*linesz, sim->addr_to_mem(addr), linesz);
+  if(!memcmp(datas + row*linesz, empty_block, linesz)) {
+    tags[row] = (addr >> idx_shift) | VALID;
+  } else {
+    tags[row] = (addr >> idx_shift) | VALID | TAGFLAG;
+  }
+}
+
+void tag_cache_sim_t::writeback(size_t row) {
+  tags[row] = 0;
+  if((tags[row] & (VALID|DIRTY)) != (VALID|DIRTY)) return;
+  if(writeback_avoid(row)) return;
+  //uint64_t addr = tags[row] << idx_shift;
+  //memcpy(sim->addr_to_mem(addr), datas + row*linesz, linesz);
+}
+
+void tag_cache_sim_t::verify(size_t row) {
+  uint64_t addr = tags[row] << idx_shift;
+  assert(!memcmp(sim->addr_to_mem(addr), datas+row*linesz, linesz));
+}
+
+uint64_t tag_cache_sim_t::read(uint64_t addr, uint64_t &data, uint8_t fetch) {
+  size_t row;
+  uint64_t *tag = check_tag(addr, row);
+  if(tag == NULL) {
+    if(!fetch) return 0;
+    tag = victimize(addr, row);
+    writeback(row);
+    refill(addr, row);
+  }
+  if((*tag) & TAGFLAG) {        // read data
+    data = *(uint64_t *)(datas + data_row_addr(addr, row));
+  } else {                      // cached but empty
+    data = 0;
+  }
+  return *tag;
+}
+
+uint64_t tag_cache_sim_t::write(uint64_t addr, uint64_t data, uint64_t mask) {
+  uint64_t rdata, wdata;
+  size_t row;
+  uint64_t *tag;
+  read(addr, rdata, 1);         // force fetch+read
+  tag = check_tag(addr, row);   // get tag and row
+  wdata = (rdata & ~mask) | (data & mask); // get the udpate data
+  if(rdata != wdata) {                     // update
+    *(uint64_t *)(datas + data_row_addr(addr, row)) = wdata;
+    *tag |= DIRTY;
+    if(!memcmp(datas + row*linesz, empty_block, linesz)) {
+      *tag &= ~TAGFLAG;
+    } else {
+      *tag |= TAGFLAG;
+    }
+  }
+  verify(row);
+  return *tag;
+}
+
+uint64_t tag_cache_sim_t::create(uint64_t addr, uint64_t data, uint64_t mask) {
+  uint64_t rdata, wdata;
+  size_t row;
+  uint64_t *tag = check_tag(addr, row);
+  if(tag == NULL) {
+    tag = victimize(addr, row);
+    writeback(row);
+    memcpy(datas + row*linesz, empty_block, linesz); // empty block
+    *tag |= (addr >> idx_shift) | VALID | DIRTY;
+    rdata = 0;
+  } else {
+    rdata = *(uint64_t *)(datas + data_row_addr(addr, row));
+  }
+  wdata = (rdata & ~mask) | (data & mask); // get the udpate data
+  if(rdata != wdata) {                     // update
+    *(uint64_t *)(datas + data_row_addr(addr, row)) = wdata;
+    *tag |= DIRTY;
+    if(!memcmp(datas + row*linesz, empty_block, linesz)) {
+      *tag &= ~TAGFLAG;
+    } else {
+      *tag |= TAGFLAG;
+    }
+  }
+  verify(row);
+  return *tag;
 }
 
 // ----------------- separate tag table class ------------------- //
 
-tag_table_sim_t::tag_table_sim_t(size_t sets, size_t ways, size_t linesz, size_t tagsz, const char* name, const sim_t* sim)
+tag_table_sim_t::tag_table_sim_t(size_t sets, size_t ways, size_t linesz, size_t tagsz, const char* name, sim_t* sim)
   : tag_cache_sim_t(sets, ways, linesz, tagsz, name, sim)
 {
 }
 
 // ----------------- separate tag map class --------------------- //
-tag_map_sim_t::tag_map_sim_t(size_t sets, size_t ways, size_t linesz, const char* name, const sim_t* sim)
+tag_map_sim_t::tag_map_sim_t(size_t sets, size_t ways, size_t linesz, const char* name, sim_t* sim)
   : tag_cache_sim_t(sets, ways, linesz, 0, name, sim)
 {
 }
 
 // ----------------- unified tag cache class -------------------- //
-unified_tag_cache_sim_t::unified_tag_cache_sim_t(size_t sets, size_t ways, size_t linesz, size_t tagsz, const char* name, const sim_t* sim)
+unified_tag_cache_sim_t::unified_tag_cache_sim_t(size_t sets, size_t ways, size_t linesz, size_t tagsz, const char* name, sim_t* sim)
   : tag_cache_sim_t(sets, ways, linesz, tagsz, name, sim)
 {
 }
--- riscv/tagcachesim.h
+++ riscv/tagcachesim.h
@@ -6,14 +6,15 @@
 #include "memtracer.h"
 #include "cachesim.h"
 #include "sim.h"
-#include <vector>
 
 class tag_cache_sim_t : public cache_sim_t
 {
  public:
-  tag_cache_sim_t(size_t sets, size_t ways, size_t linesz, size_t tagsz, const char* name, const sim_t* sim);
+  tag_cache_sim_t(size_t sets, size_t ways, size_t linesz, size_t tagsz, const char* name, sim_t* sim);
   tag_cache_sim_t(const tag_cache_sim_t& rhs);
   virtual ~tag_cache_sim_t();
+
+  virtual uint64_t access(uint64_t addr, size_t bytes, bool store) = 0;
 
  protected:
   size_t tagsz;
@@ -21,29 +22,46 @@ class tag_cache_sim_t : public cache_sim_t
   uint8_t *datas;
   tag_cache_sim_t* tag_map;
 
-  const sim_t *sim;
+  sim_t *sim;
 
   virtual void init();
+
+  static const uint64_t TAGFLAG = 1ULL << 61;
+  uint64_t* check_tag(uint64_t addr, size_t &row);
+  uint64_t* victimize(uint64_t addr, size_t &row);
+  void refill(uint64_t addr, size_t row);
+  void writeback(size_t row);
+  size_t data_row_addr(uint64_t addr, size_t row) {
+    return row*linesz + (addr%linesz & ~(uint64_t)(0x07));
+  }
+  virtual uint8_t writeback_avoid(size_t row) {
+    return 0;
+  }
+  void verify(size_t row);  // verify that the row data match with actual memory data
+
+  uint64_t read(uint64_t addr, uint64_t &data, uint8_t fetch);
+  uint64_t write(uint64_t addr, uint64_t data, uint64_t mask);
+  uint64_t create(uint64_t addr, uint64_t data, uint64_t mask);
 };
 
 class tag_table_sim_t : public tag_cache_sim_t
 {
  public:
-  tag_table_sim_t(size_t sets, size_t ways, size_t linesz, size_t tagsz, const char* name, const sim_t* sim);
+  tag_table_sim_t(size_t sets, size_t ways, size_t linesz, size_t tagsz, const char* name, sim_t* sim);
   static tag_cache_sim_t* construct(const char* config, const char* name);
 };
 
 class tag_map_sim_t : public tag_cache_sim_t
 {
  public:
-  tag_map_sim_t(size_t sets, size_t ways, size_t linesz, const char* name, const sim_t* sim);
+  tag_map_sim_t(size_t sets, size_t ways, size_t linesz, const char* name, sim_t* sim);
   static tag_cache_sim_t* construct(const char* config, const char* name);
 };
 
 class unified_tag_cache_sim_t : public tag_cache_sim_t
 {
  public:
-  unified_tag_cache_sim_t(size_t sets, size_t ways, size_t linesz, size_t tagsz, const char* name, const sim_t* sim);
+  unified_tag_cache_sim_t(size_t sets, size_t ways, size_t linesz, size_t tagsz, const char* name, sim_t* sim);
   static tag_cache_sim_t* construct(const char* config, const char* name);
 };
 

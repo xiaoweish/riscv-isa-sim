cd68e71774892c8c312f7d0ce8f9329cb187ed5b
HelloKayT <katiemlim@gmail.com>
2017-07-05 10:46:52 +0100

    Modifying the CSR read/write immediate instructions to clear tags

--- riscv/insns/csrrci.h
+++ riscv/insns/csrrci.h
@@ -1,7 +1,7 @@
 int csr = validate_csr(insn.csr(), true);
 word_t old_tag;
 word_t old = p->get_csr(csr, &old_tag);
-p->set_csr(csr, old & ~(word_t)insn.rs1(), old_tag);
+p->set_csr(csr, old & ~(word_t)insn.rs1(), 0);
 WRITE_RD(reg_t(sext_xlen(old)));
 switch (csr) {
    case CSR_MEPC:

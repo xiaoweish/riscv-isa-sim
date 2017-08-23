16d45fa96ca3151349e27266a5e9338d38ff7148
HelloKayT <katiemlim@gmail.com>
2017-08-23 16:31:09 +0100

    Print statements for logging branch targets

--- riscv/insns/bltu.h
+++ riscv/insns/bltu.h
@@ -4,4 +4,6 @@ if(RS1.data < RS2.data) {
    tag = get_field(tagctrl, TMASK_CFLOW_DIR_TGT);
    set_pc(BRANCH_TARGET);
    npc.tag = tag;
+   if (STATE.prv == PRV_U)
+      fprintf(stderr, "pc: 0x%016" PRIx64 " target: 0x%016" PRIx64 "\n", pc.data, jmp_target);
 }
--- riscv/insns/jalr.h
+++ riscv/insns/jalr.h
@@ -12,4 +12,5 @@ npc.tag = tag;
 WRITE_RD(tmp);
 tag = get_field(tagctrl, TMASK_JMP_PROP);
 WRITE_RD_TAG(tag);
-
+if (STATE.prv == PRV_U)
+   fprintf(stderr, "pc: 0x%016" PRIx64 " target: 0x%016" PRIx64 "\n", pc.data, jmp_target);

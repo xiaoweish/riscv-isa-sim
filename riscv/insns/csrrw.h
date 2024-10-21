9e061fc443d97ea1e7a30a3c4885b9145c912805
Gabriel Ibarra <gabriel.ibarra@broadcom.com>
2024-10-21 13:03:48 -0500

    fix issue with SMCLIC mode and SUCLIC

--- riscv/insns/csrrw.h
+++ riscv/insns/csrrw.h
@@ -1,5 +1,21 @@
 int csr = validate_csr(insn.csr(), true);
-if (csr == CSR_USCRATCHCSWL) {
+if (csr == CSR_MSCRATCHCSW) {
+  if ((p->get_state()->prv) == ((p->get_csr(CSR_MSTATUS, insn, false, true) & MSTATUS_MPP) >> MSTATUS_MPP_LSB)) {
+    WRITE_RD(sext_xlen(RS1));
+  } else {
+    reg_t old = p->get_csr(CSR_MSCRATCH, insn, true);
+    p->put_csr(CSR_MSCRATCH, RS1);
+    WRITE_RD(sext_xlen(old));
+  }
+} else if (csr == CSR_MSCRATCHCSWL) {
+  if (((p->get_csr(CSR_MCAUSE, insn, false, true) & MCAUSE_MPIL) == 0) !=
+      ((p->get_csr(CSR_MINTSTATUS, insn, false, true) & MINTSTATUS_MIL) == 0))
+  {
+    reg_t old = p->get_csr(CSR_MSCRATCH, insn, true);
+    p->put_csr(CSR_MSCRATCH, RS1);
+    WRITE_RD(sext_xlen(old));
+  }
+}  else if (csr == CSR_USCRATCHCSWL) {
   if (((p->get_csr(CSR_UCAUSE, insn, false, true) & UCAUSE_UPIL) == 0) !=
       ((p->get_csr(CSR_UINTSTATUS, insn, false, true) & UINTSTATUS_UIL) == 0))
   {
